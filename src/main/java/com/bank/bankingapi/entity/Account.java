8717cef7ecf3781f6d397e01ce08f71fa5b0f3db
Mohamed Reda Baghlal <baghlalmohamedreda.25@ump.ac.ma>
2026-09-23 17:37:45 +0000

    feat: add getters ans setters to acount entity

--- src/main/java/com/bank/bankingapi/entity/Account.java
+++ src/main/java/com/bank/bankingapi/entity/Account.java
@@ -14,4 +14,30 @@ public class Account {
     private String ownerName;
     private double balance;
 
+    public Long getId(){
+        return id;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+    public void setOwnerName(String ownerName){
+        this.ownerName=ownerName;
+    }
+    public double getBalance(){
+        return balance;
+    }
+    public void setBalance(double balance){
+        this.balance=balance;
+    }
+
+
 }
